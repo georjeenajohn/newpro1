f844fd91ae72b5226dbc36184d3f4271e5d810d4
georjeenajohn <johnmary2919@gmail.com>
2021-11-11 20:06:41 +0530

    first commit

---
+++ src/org/test/gittest.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class gittest {
+	
+	private void Studentname() {
+		 System.out.println("Studentname : jeena");
+		
+
+	}
+	 private void Studentid() {
+		 System.out.println("Studentid : 179");
+		
+	}
+
+}
